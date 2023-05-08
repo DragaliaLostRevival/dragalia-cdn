2b8f330bf3de979b42caa8053a12607c16ff598a
Jay Malhotra <5047192+SapiensAnatis@users.noreply.github.com>
2023-05-08 22:57:19 +0100

    Add modding support 2

--- src/dragalia_cdn/server.rs
+++ src/dragalia_cdn/server.rs
@@ -1,11 +1,15 @@
-use std::{fs, process};
+use std::fs;
 use std::net::SocketAddr;
-use std::path::{PathBuf};
+use std::path::PathBuf;
 use std::sync::Arc;
+use reqwest::Url;
+use tokio::io::{self, AsyncReadExt};
+use tokio::fs::File;
 use axum::{body, Router};
-use axum::body::{Empty, Full};
+use axum::body::Full;
+use axum::body::Empty;
 use axum::extract::{Path, State};
-use axum::http::{header, HeaderValue, StatusCode};
+use axum::http::{header, HeaderValue, StatusCode, header::HeaderMap};
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum_server::tls_rustls::RustlsConfig;
@@ -17,207 +21,6 @@ use super::config::ServerConfig;
 
 pub async fn start_server() {
 
-    let server_config: ServerConfig;
-
-    if std::path::Path::new("config.json").exists() {
-        server_config = match fs::read_to_string("config.json") {
-            Ok(file) => match serde_json::from_str(&file) {
-                Ok(config) => config,
-                Err(e) => {
-                    panic!("Failed to deserialize config.json: {:?}", e);
-                }
-            }
-            Err(e) => {
-                panic!("Failed to read config.json: {:?}", e);
-            }
-        };
-    } else {
-        server_config = ServerConfig::new();
-        let serialized_config = serde_json::to_string_pretty(&server_config).unwrap();
-        fs::write("config.json", serialized_config).unwrap_or_else(|e| {
-            panic!("Failed to write new config to config.json: {:?}", e);
-        })
-    }
-
-    let shared_config = Arc::new(server_config);
-
-    if shared_config.locations.assetbundles.is_empty() {
-        panic!("No asset folders configured. Please edit config.json to point to the location of your assets.");
-    }
-
-    if shared_config.locations.manifests.is_empty() {
-        warn!("No manifest folders configured. The server will be unable to serve file lists for fresh downloads.");
-    }
-
-    let addr = SocketAddr::new("0.0.0.0".parse().unwrap(), shared_config.server.port);
-
-    let port = shared_config.server.port;
-    let use_https = shared_config.server.https.enabled;
-    let cert_path = shared_config.server.https.cert.clone();
-    let key_path = shared_config.server.https.key.clone();
-
-    let app = Router::new()
-        .route("/info", get(get_info))
-        .route("/dl/manifests/*path", get(get_manifest))
-        .route("/dl/assetbundles/*path", get(get_assetbundle))
-        .with_state(shared_config);
-
-
-   if use_https {
-       let tls_config = Some(RustlsConfig::from_pem_file(
-           cert_path,
-           key_path
-       ).await.unwrap_or_else(|e| {
-           panic!("Failed to load TLS config: {}", e);
-       }));
-
-       info!("Starting HTTPS server on port {}!", port);
-
-       axum_server::bind_rustls(addr, tls_config.unwrap())
-           .serve(app.into_make_service())
-           .await
-           .unwrap_or_else(|e| {
-               panic!("Failed to start HTTPS server: {:?}", e);
-           });
-   } else {
-       info!("Starting HTTP server on port {}!", port);
-
-       axum_server::bind(addr)
-           .serve(app.into_make_service())
-           .await
-           .unwrap_or_else(|e| {
-               panic!("Failed to start HTTP server: {:?}", e);
-           });
-   }
-
-    info!("Server is shutting down...");
-}
-
-async fn get_info() -> &'static str {
-    "omg cross-platform rust"
-}
-
-async fn get_assetbundle(State(state): State<Arc<ServerConfig>>, Path(path): Path<String>, headers: HeaderMap) -> impl IntoResponse {
-    lazy_static! {
-        static ref ASSETBUNDLE_REGEX: Regex = Regex::new(r"^(Android|iOS)/.*([A-Z2-7=]{2})/([A-Z2-7=]{52})$").unwrap();
-    }
-
-    if !ASSETBUNDLE_REGEX.is_match(&path) {
-        return Response::builder().status(StatusCode::FORBIDDEN).body(body::boxed(Empty::new())).unwrap()
-    }
-
-    let captures = ASSETBUNDLE_REGEX.captures(&path).unwrap();
-    
-    match get_local_file(&state.locations.manifests, &captures).await {
-        Some(response) => response,
-        None => match get_remote_file(headers, &captures).await {
-            Some(response) => response,
-            None => Response::builder().status(StatusCode::NOT_FOUND).body(body::boxed(Empty::new())).unwrap()
-        }
-    }
-}
-
-async fn get_manifest(State(state): State<Arc<ServerConfig>>, Path(path): Path<String>, headers: HeaderMap) -> impl IntoResponse {
-    lazy_static! {
-        static ref MANIFEST_REGEX: Regex = Regex::new(r"^(Android|iOS)/([A-Za-z0-9]{1,16})/(assetbundle\.(?:(?:en_us|en_eu|zh_cn|zh_tw)\.)?manifest)$").unwrap();
-    }
-
-    if !MANIFEST_REGEX.is_match(&path) {
-        return Response::builder().status(StatusCode::FORBIDDEN).body(body::boxed(Empty::new())).unwrap()
-    }
-
-    let captures = MANIFEST_REGEX.captures(&path).unwrap();
-
-    match get_local_file(&state.locations.manifests, &captures).await {
-        Some(response) => response,
-        None => match get_remote_file(headers, &captures).await {
-            Some(response) => response,
-            None => Response::builder().status(StatusCode::NOT_FOUND).body(body::boxed(Empty::new())).unwrap()
-        }
-    }
-}
-
-async fn get_local_file(dirs: &Vec<String>, captures: &Captures<'_>) -> Option<Response> {
-    for path in dirs {
-        let mut base_path = PathBuf::new();
-
-        base_path.push(path);
-        base_path.push(&captures[2]);
-        base_path.push(&captures[3]);
-
-        if base_path.exists() {
-            let response = match read_file_into_response(base_path).await {
-                Ok(content) => content,
-                Err(e) => {
-                    error!("Failed to read found file: {:?}", e);
-                    return None;
-                }
-            };
-
-            return Some(response);
-        }
-    }
-
-    info!("Could not find local file for request path {}.", &captures[0]);
-    return None;
-}
-
-async fn get_remote_file(headers: HeaderMap, captures: &Captures<'_>) -> Option<Response> {
-    let base_url_string = match headers.get("reliable_token") {
-        Some(str) => str,
-        None => {
-            error!("Missing reliable_token header. Please upgrade DragaliPatch to the latest version.");
-            return None;
-        }
-    }.to_str().unwrap();
-    
-    let base_url = Url::parse(base_url_string).expect("Header URL should be valid");
-    let url = base_url.join(&captures[2]).unwrap().join(&captures[3]).unwrap();
-
-    let res = match reqwest::get(url).await {
-        Ok(res) => res,
-        Err(e) => {
-            error!("Request failed: {:?}", e);
-            return None;
-        }
-    };
-    
-    if !res.status().is_success() {
-        warn!("Receieved non-success status code {} on {:?}", res.status(), res.url());
-        return None;
-    }
-    
-    let content = match res.bytes().await{
-        Ok(bytes) => bytes,
-        Err(e) => {
-            error!("Failed to read response body: {:?}", e);
-            return None;
-        }};
-    
-    Some(Response::builder().status(StatusCode::OK).header(
-            header::CONTENT_TYPE,
-            HeaderValue::from_str("application/octet-stream")
-                .unwrap()
-        )
-        .body(body::boxed(Full::from(content)))
-        .unwrap())
-}
-
-async fn read_file_into_response(path: PathBuf) -> Result<Response, io::Error> {
-    let mut file = File::open(path).await?;
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).await?;
-
-    Ok(Response::builder()
-        .status(StatusCode::OK)
-        .header(
-            header::CONTENT_TYPE,
-            HeaderValue::from_str("application/octet-stream")
-                .unwrap()
-        )
-        .body(body::boxed(Full::from(buffer)))
-        .unwrap())
-}
     let server_config: ServerConfig;
 
     if std::path::Path::new("config.json").exists() {
